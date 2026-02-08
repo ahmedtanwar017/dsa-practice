0e0c4dd8c418dcb552e7707246f4ea663e38f968
Ahmed Tanwar <ahmedtanwar017@gmail.com>
2026-02-09 03:22:23 +0530

    Two Pointer Practice

--- twoPointer.js
+++ twoPointer.js
@@ -1,37 +1,35 @@
-const array = [12,3,1,22,5];
+const array = [12, 3, 1, 22, 5];
 
-function Sorting(array){
-
-    for(let i = 0; i < array.length; i++){
-
-        for(let j = 0; j < array.length - i - 1; j++){
-
-            if(array[j] > array[j + 1]){
-
-                let temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-
-            }
-        }
+function Sorting(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
-    return array;
+  }
+  return array;
 }
+// console.log(Sorting(array));
 
-console.log(Sorting(array));
+function TwoSum(target, array) {
+  Sorting(array);
+  let left = 0;
+  let right = array.length - 1;
+  let sum = "";
+  while (left < right) {
+    sum = array[left] + array[right];
 
-
-// function TwoSum(target,arr){
-//     let left = arr[0];
-//     let right = arr.lenght - 1;
-//     let result = "";
-//     for(let i = left; i < right; i++){
-//         for(let j = right;j < left; right--){
-//             result = i + j
-//             if(result == target){
-//                 return result
-//             }
-//         }
-//     }
-// }
-// TwoSum(4,array)
+    if (sum == target) {
+      return [array[left], array[right]];
+    } else if (sum < target) {
+        left++;
+    } else{
+        right--;
+    }
+  }
+  return "No Pair Found"
+}
+console.log(TwoSum(8, array));
