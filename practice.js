86df3be2636fee969766202fd659f5e5234e0c37
Ahmed Tanwar <ahmedtanwar017@gmail.com>
2026-02-24 06:35:04 +0530

    Improvements

--- practice.js
+++ practice.js
@@ -1,86 +1,130 @@
-function HelloWorld() {
-  console.log("Hello World");
+// // function HelloWorld() {
+// //   console.log("Hello World");
+// // }
+// // HelloWorld();
+
+// // function Sum(a, b) {
+// //   let c = a + b;
+// //   console.log("Sum is:", c);
+// // }
+// // Sum(-222, 223);
+
+// // function AreaofRectangle(l, b) {
+// //   if (l <= 0 || b <= 0) {
+// //     console.error("Value should be greater than 0");
+// //     return; // STOP execution
+// //   }
+
+// //   const area = l * b;
+// //   console.log("Area Of Rectangle:", area);
+// // }
+// // AreaofRectangle(4, -14);
+
+// // let array = [1, 2, 3, 4, 5];
+// // function Sum(arr) {
+// //   let sum = 0;
+// //   for (let i = 0; i < arr.length; i++) {
+// //     sum = sum + arr[i];
+// //   }
+// //   return sum;
+// // }
+// // console.log(Sum(array));
+
+// // function ChkEvenOdd(num) {
+// //   if (num % 2 == 0) {
+// //     console.log(`Even number ${num}`);
+// //   } else {
+// //     console.log(`Num is Odd ${num}`);
+// //   }
+// // }
+// // ChkEvenOdd(-2);
+
+// // function SmallestNum(a, b, c) {
+// //   if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number") {
+// //     console.log("Invalid Number");
+// //     return;
+// //   }
+
+// //   if (a < b && a < c) {
+// //     return `A is Smallest ${a}`;
+// //   } else if (b < c && b < a) {
+// //     return `B is Smallest ${b}`;
+// //   } else if (c < a && c < b) {
+// //     return `C is Smallest ${c}`;
+// //   } else {
+// //     return "Some values are equal";
+// //   }
+// // }
+
+// // console.log(SmallestNum(-1, 3, 1));
+
+// // // Reverse a String
+// // function ReverseString(str) {
+// //   if(typeof str !== "string"){
+// //     throw new Error("Only String Allowed")
+// //   }
+// //   let rev = ""
+// //   let s = 0;
+// //   for (let i = str.length - 1; i >= s; i--) {
+// //       rev = rev + str[i]
+// //   }
+// //   return rev
+// // }
+// // console.log(ReverseString("array"));
+
+// // function Uturn(str){
+// //   if(typeof str !== "string"){
+// //     throw new Error("Only String Allowed")
+// //   }
+// //   const Split = str.split("");
+// //   const Reverse = Split.reverse()
+// //   const Result = Reverse.join("")
+// //   return Result
+// // }
+// // console.log(Uturn("Car"));
+
+// // a = [1,9,2,3,4,5,3,3]
+// // count = ""
+// // function Repetition(arr){
+// //   for(let i = 0; i < arr.length; i++){
+// //     for(let j = i + 1; j < arr.length; j++){
+// //       if(arr[i] === arr[j]){
+// //         return arr[i];   // first repeating number
+// //       }
+// //     }
+// //   }
+// //   return null;  // if no repetition
+// // }
+
+// // console.log(Repetition(a));
+
+// function Factorial(num){
+//   let sum = 1
+//   for(let i = 1;i <= num;i++){
+//     sum = sum * i
+//   }
+//   return sum
+// }
+// console.log(Factorial(3));
+// // 3! = 3*2*1 = 6*1 = 6
+
+// function leapyear(year) {
+//   if (year % 2 == 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(leapyear(2023));
+
+let arr = [1, 2, 2, 3];
+
+let map = new Map();
+
+for (let num of arr) {
+  map.set(num, (map.get(num) || 0) + 1);
 }
-HelloWorld();
 
-function Sum(a, b) {
-  let c = a + b;
-  console.log("Sum is:", c);
+for (let [key, value] of map) {
+  console.log(`${key} is ${value}`);
 }
-Sum(-222, 223);
-
-function AreaofRectangle(l, b) {
-  if (l <= 0 || b <= 0) {
-    console.error("Value should be greater than 0");
-    return; // STOP execution
-  }
-
-  const area = l * b;
-  console.log("Area Of Rectangle:", area);
-}
-AreaofRectangle(4, -14);
-
-let array = [1, 2, 3, 4, 5];
-function Sum(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
-  return sum;
-}
-console.log(Sum(array));
-
-function ChkEvenOdd(num) {
-  if (num % 2 == 0) {
-    console.log(`Even number ${num}`);
-  } else {
-    console.log(`Num is Odd ${num}`);
-  }
-}
-ChkEvenOdd(-2);
-
-function SmallestNum(a, b, c) {
-  if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number") {
-    console.log("Invalid Number");
-    return;
-  }
-
-  if (a < b && a < c) {
-    return `A is Smallest ${a}`;
-  } else if (b < c && b < a) {
-    return `B is Smallest ${b}`;
-  } else if (c < a && c < b) {
-    return `C is Smallest ${c}`;
-  } else {
-    return "Some values are equal";
-  }
-}
-
-console.log(SmallestNum(-1, 3, 1));
-
-// Reverse a String
-function ReverseString(str) {
-  if(typeof str !== "string"){
-    throw new Error("Only String Allowed")
-  }
-  let rev = ""
-  let s = 0;
-  for (let i = str.length - 1; i >= s; i--) {
-      rev = rev + str[i]
-  }
-  return rev
-}
-console.log(ReverseString("array"));
-
-function Uturn(str){
-  if(typeof str !== "string"){
-    throw new Error("Only String Allowed")
-  }
-  const Split = str.split("");
-  const Reverse = Split.reverse()
-  const Result = Reverse.join("")
-  return Result
-}
-console.log(Uturn("Car"));
-
-
--- twoPointer.js
+++ twoPointer.js
@@ -1,55 +1,142 @@
-// const array = [12, 3, 1, 22, 5];
+// // // const array = [12, 3, 1, 22, 5];
 
-function Sorting(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        let temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
-    }
-  }
-  return array;
-}
-// // console.log(Sorting(array));
+// // function Sorting(array) {
+// //   for (let i = 0; i < array.length; i++) {
+// //     for (let j = 0; j < array.length - i - 1; j++) {
+// //       if (array[j] > array[j + 1]) {
+// //         let temp = array[j];
+// //         array[j] = array[j + 1];
+// //         array[j + 1] = temp;
+// //       }
+// //     }
+// //   }
+// //   return array;
+// // }
+// // // console.log(Sorting(array));
 
-// function TwoSum(target, array) {
-//   Sorting(array);
-//   console.log(array);
-//   let left = 0;
-//   let right = array.length - 1;
-//   let sum = "";
-//   while (left < right) {
-//     sum = array[left] + array[right];
+// // // function TwoSum(target, array) {
+// // //   Sorting(array);
+// // //   console.log(array);
+// // //   let left = 0;
+// // //   let right = array.length - 1;
+// // //   let sum = "";
+// // //   while (left < right) {
+// // //     sum = array[left] + array[right];
 
-//     if (sum == target) {
-//       return [array[left], array[right]];
-//     } else if (sum < target) {
-//         left++;
-//     } else{
-//         right--;
+// // //     if (sum == target) {
+// // //       return [array[left], array[right]];
+// // //     } else if (sum < target) {
+// // //         left++;
+// // //     } else{
+// // //         right--;
+// // //     }
+// // //   }
+// // //   return "No Pair Found"
+// // // }
+// // // console.log(TwoSum(8, array));
+
+// // // let duplicate = [3, 3, 2, 2, 2, 3, 1, 1];
+// // // // [1,1,2,2,2,3,3,3]
+// // // function Duplicates(arr) {
+// // //   Sorting(arr);
+// // //   let i = 0;
+// // //   let j = 1;
+// // //   while (j < arr.length) {
+// // //     if (arr[j] == arr[i]) {
+// // //       j++;
+// // //     } else if (arr[j] !== arr[i]) {
+// // //       arr[i + 1] = arr[j];
+// // //       i++;
+// // //       j++;
+// // //     }
+// // //   }
+// // //   return arr;
+// // // }
+// // // console.log(Duplicates(duplicate));
+
+// // let chk = [-4,-2,0,3,5]
+// // const sqaure = chk.forEach(function(num){
+// //   const sqaure = num ** 2
+// //   return sqaure
+// // })
+
+// let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// function removeDuplicates(nums) {
+//     let i = 0;  // slow pointer
+
+//     for (let j = 1; j < nums.length; j++) {  // fast pointer
+//         if (nums[j] !== nums[i]) {
+//             i++;
+//             nums[i] = nums[j];
+//         }
+//     }
+
+//     return i + 1;
+// }
+// console.log(removeDuplicates(nums));
+
+// let nums1 = [1, 3];
+// let nums2 = [2];
+// var findMedianSortedArrays = function (nums1, nums2) {
+//   const array = nums1.concat(nums2);
+//   for (let i = 0; i < array.length; i++) {
+//     if (i < array.lenght) {
+//       const TotalL = nums1.length + nums2.length;
+//       let M = TotalL / 2;
+//       return M;
 //     }
 //   }
-//   return "No Pair Found"
-// }
-// console.log(TwoSum(8, array));
+// };
 
-let duplicate = [3, 3, 2, 2, 2, 3, 1, 1];
-// [1,1,2,2,2,3,3,3]
-function Duplicates(arr) {
-  Sorting(arr);
-  let i = 0;
-  let j = 1;
-  while (j < arr.length) {
-    if (arr[j] == arr[i]) {
-      j++;
-    } else if (arr[j] !== arr[i]) {
-      arr[i + 1] = arr[j];
-      i++;
-      j++;
+// console.log(findMedianSortedArrays(nums1, nums2));
+
+// let nums = [3,2,2,3];
+// let nums2 = [0,1,2,2,3,0,4,2];
+// let value = 3;
+// let value2 = 2;
+
+// var removeElement = function(nums, val) {
+//   let k = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== val) {   // ✅ use val here
+//       nums[k] = nums[i];
+//       k++;
+//     }
+//   }
+
+//   return k;
+// };
+
+// console.log(removeElement(nums, value));
+// console.log(removeElement(nums2, value2));
+
+// console.log(nums);
+// console.log(nums2);
+
+let a = [-4,-1,0,3,10];
+const square = a.map((n) => n ** 2);
+
+var sortedSquares = function (arr) {
+  if (arr.length <= 1) return arr; // base case
+
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  return merge(sortedSquares(left), sortedSquares(right));
+};
+function merge(left, right) {
+  let temp = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      temp.push(left.shift());
+    } else {
+      temp.push(right.shift());
     }
   }
-  return arr;
+
+  return [...temp, ...left, ...right];
 }
-console.log(Duplicates(duplicate));
+console.log(sortedSquares(square));
